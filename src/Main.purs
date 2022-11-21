55ec0cf0d0c355fb2775c3fc1df64f71199b7d9a
carp-sushi <105142707+carp-sushi@users.noreply.github.com>
2022-11-21 07:08:13 -0700

    add max, min to main

--- src/Main.purs
+++ src/Main.purs
@@ -1,12 +1,12 @@
 module Main where
 
-import Prelude (Unit, discard, map, ($), (+))
+import Prelude (Unit, discard, map, ($), (+), (*), (<<<))
 
 import Data.Semigroup (class Semigroup, (<>))
 import Effect (Effect)
 import Effect.Console (log, logShow)
 
-import Data.Tree (Tree, insert, invert, mkTree, remove, search, toArray)
+import Data.Tree (Tree, insert, invert, max, min, mkTree, remove, search, toArray)
 
 -- | Need to wrap Int type to implement semigroup.
 newtype MyInt = MyInt Int
@@ -28,22 +28,31 @@ main = do
   let numbers = [ 4, 2, 1, 3, 7, 6, 9 ]
       tree = mkTree numbers
       inverted = invert tree
+
   log "Numbers"
   logShow numbers
   log "Tree"
   logShow tree
-  log "Invert Tree"
+
+  log "Insert 5, 8"
+  logShow $ (insert 8 <<< insert 5) tree
+  log "Search 7"
+  logShow $ search 7 tree
+  log "Remove 7"
+  logShow $ remove 7 tree
+
+  log "Max"
+  logShow $ max tree
+  log "Min"
+  logShow $ min tree
+
+  log "Inverted Tree"
   logShow inverted
   log "Inverted As Array"
   logShow $ toArray inverted
-  log "Insert 5"
-  logShow $ insert 5 tree
-  log "Remove 7"
-  logShow $ remove 7 tree
-  log "Search 7"
-  logShow $ search 7 tree
-  log "Tree Sum"
-  logShow tree
-  logShow inverted
+
+  log "Tree Sum (tree + inverted)"
   logShow $ treeSum tree inverted
+  log "Tree x 2"
+  logShow $ map (_*2) tree
 
